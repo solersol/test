454d0ccebcd0c99af26af297f8c6d7fbb7f3eec6
solersol <ttpage1tk@gmail.com>
2024-09-03 02:35:35 +0300

    Vdf

--- src/sections/NewSection.jsx
+++ src/sections/NewSection.jsx
@@ -4,7 +4,7 @@ const NewSection = () => {
   return (
     <section id="roadmap" className="w-full h-screen flex flex-col items-center">
       {/* Title at the Top Center */}
-      <h1 className="text-8xl  font-anton font-bold mt-8">$COW ON WHEELS </h1>
+      <h1 className="text-8xlg  font-anton font-bold mt-8">$COW ON WHEELS </h1>
       
       {/* Additional Content Here */}
       <div className="flex flex-col items-center justify-center flex-1">
