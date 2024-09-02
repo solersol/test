a3735838b9e6ea856ade3f20e4b85c08035cf79d
solersol <ttpage1tk@gmail.com>
2024-09-03 02:40:30 +0300

    vfd

--- src/sections/NewSection.jsx
+++ src/sections/NewSection.jsx
@@ -5,6 +5,7 @@ const NewSection = () => {
     <section id="roadmap" className="w-full h-screen flex flex-col items-center">
       {/* Title at the Top Center */}
       <h1 className="text-8xl  font-anton font-bold mt-8">$COW ON WHEELS </h1>
+      <h1 className="text-8xl  font-anton font-bold mt-8">CA : 5QWVLACwiScc6V2PRoj7zXjLgGH55GDsJaE28HeApump </h1>
       
       {/* Additional Content Here */}
       <div className="flex flex-col items-center justify-center flex-1">
--- src/sections/StickyFooter.jsx
+++ src/sections/StickyFooter.jsx
@@ -10,7 +10,7 @@ const StickyFooter = () => {
         <a href="https://x.com/CowOnWheelsSol">
           <img src={img1} alt="Image 1" className="w-16 h-16 sm:w-12 sm:h-12 transition-transform transform hover:scale-125" />
         </a>
-        <a href="https://pump.fun/">
+        <a href="https://pump.fun/5QWVLACwiScc6V2PRoj7zXjLgGH55GDsJaE28HeApump">
           <img src={img2} alt="Image 2" className="w-16 h-16 sm:w-12 sm:h-12 transition-transform transform hover:scale-125" />
         </a>
         <a href="https://t.me/COWonWHEELS">
